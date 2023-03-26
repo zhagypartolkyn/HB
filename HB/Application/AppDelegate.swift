83e3266e525f4b56d5f0040a1a73700162764622
Tolkyn Zhagypar <tolkyn.zhagipar@gmail.com>
2023-03-26 17:19:19 +0100

    feature: add application needed files

--- HB/Application/AppDelegate.swift
+++ HB/Application/AppDelegate.swift
@@ -1,19 +1,63 @@
-//
-//  AppDelegate.swift
-//  HB
-//
-//  Created by Толкын Жагыпар on 13.06.2023.
-//
+
 
 import UIKit
+import Firebase
+import GooglePlaces
+import GoogleSignIn
+import IQKeyboardManager
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // MARK: - Variables
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator!
+    
+    let gcmMessageIDKey = "gcm.message_id"
+    static var isToken: String?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        IQKeyboardManager.shared().isEnabled = true
+        IQKeyboardManager.shared().isEnableAutoToolbar = false
+        IQKeyboardManager.shared().shouldResignOnTouchOutside = true
+        
+        IQKeyboardManager.shared().enabledToolbarClasses.add(SignUpViewController.self)
+        IQKeyboardManager.shared().enabledToolbarClasses.add(SignEmailViewController.self)
+        IQKeyboardManager.shared().enabledToolbarClasses.add(EditProfileViewController.self)
+        
+        // TOKEN
+        Messaging.messaging().token { (token, error) in
+            if let error = error {
+                print("Error fetching remote instange ID: \(error)")
+            } else if let token = token {
+                print("Remote instance ID token: \(token)")
+                AppDelegate.isToken = token
+            }
+        }
+        
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in })
+        
+        if let isPushEnabled = UserDefaults.standard.object(forKey: "push") {
+            if isPushEnabled as! Bool {
+                UIApplication.shared.registerForRemoteNotifications()
+            } else {
+                UIApplication.shared.unregisterForRemoteNotifications()
+            }
+        } else {
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        
+        // [START set_messaging_delegate]
+        Messaging.messaging().delegate = self
+        // [END set_messaging_delegate]
+        
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GMSPlacesClient.provideAPIKey("AIzaSyAIaIwqo8rkxZVVWS3rDQoTXOdAXP0sHKY")
+        
         return true
     }
 
@@ -31,6 +75,141 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
 
+extension AppDelegate {
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID 1: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+    }
+    
+    // [START receive_message]
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID 2: \(messageID)")
+        }
+        
+        // Print full message.
+        // print(userInfo)
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    // [END receive_message]
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Unable to register for remote notifications: \(error.localizedDescription)")
+    }
+    
+    // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+    // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+    // the FCM registration token.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+        
+        // With swizzling disabled you must set the APNs token here.
+        // Messaging.messaging().apnsToken = deviceToken
+    }
+}
+
+// [START ios_10_message_handling]
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID 3: \(messageID)")
+        }
+        
+        // Print full message.
+        // print(userInfo)
+        
+        let isPushEnabled = UserDefaults.standard.bool(forKey: "push")
+        
+        // Change this to your preferred presentation option
+        if isPushEnabled {
+            completionHandler([[.alert, .sound]])
+        } else {
+            completionHandler([])
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID 4: \(messageID)")
+        }
+        
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print full message.
+        
+        // If app is active
+        if let type = userInfo["type"] as? String,
+           let linkId = userInfo["linkId"] as? String,
+           let pushType = PushType(rawValue: type) {
+            
+            let data = PushData(type: pushType, linkId: linkId)
+
+            if let initialCoordinator = appCoordinator.childCoordinators[0] as? InitialCoordinator {
+                if !initialCoordinator.childCoordinators.isEmpty {
+                    if let tabbarCoorinator = initialCoordinator.childCoordinators[0] as? TabBarCoordinator {
+                        tabbarCoorinator.openPush(data)
+                    }
+                }
+            }
+        }
+        
+        completionHandler()
+    }
+    
+    func getCurrentVC() -> UIViewController? {
+        if let initialCoordinator = appCoordinator.childCoordinators[0] as? InitialCoordinator {
+            if !initialCoordinator.childCoordinators.isEmpty {
+                if let tabbarCoorinator = initialCoordinator.childCoordinators[0] as? TabBarCoordinator {
+                    return tabbarCoorinator.currentVC()
+                } else { return nil }
+            } else { return nil }
+        } else { return nil }
+    }
+}
+// [END ios_10_message_handling]
+
+extension AppDelegate : MessagingDelegate {
+    // [START refresh_token]
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        // print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict:[String: String] = ["token": fcmToken]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    // [END refresh_token]
+}
--- HB/Application/SceneDelegate.swift
+++ HB/Application/SceneDelegate.swift
@@ -1,22 +1,41 @@
-//
-//  SceneDelegate.swift
-//  HB
-//
-//  Created by Толкын Жагыпар on 13.06.2023.
-//
+
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    private var appCoordinator : AppCoordinator!
+    
+    var pushData: PushData?
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.windowScene = windowScene
+        
+        // When you app is in closed state you should check for launch option in
+        if let response = connectionOptions.notificationResponse {
+            let userInfo = response.notification.request.content.userInfo
+            
+            if let type = userInfo["type"] as? String,
+               let linkId = userInfo["linkId"] as? String,
+               let pushType = PushType(rawValue: type) {
+                
+                pushData = PushData(type: pushType, linkId: linkId)
+            }
+        }
+            
+        
+        let appCoordinator = AppCoordinator(window: window!, pushData: pushData)
+        appCoordinator.start()
+        self.appCoordinator = appCoordinator
+        
+        // Window clone in AppDelegate for ProgressHUD (Not working on center)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.window = window
+        appDelegate.appCoordinator = appCoordinator
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -24,29 +43,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        DB.Helper.isOnline(bool: false)
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        DB.Helper.isOnline(bool: true)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        DB.Helper.isOnline(bool: false)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        DB.Helper.isOnline(bool: true)
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        DB.Helper.isOnline(bool: false)
     }
 
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+//        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+//            let url = userActivity.webpageURL,
+//            let wishIdFromUrl = url.queryParams()["wish"]
+////            let currentVC = UIApplication.topViewController()
+//            else { return }
+//
+//        print("link")
+////
+////        let vc = WishController()
+////        vc.id = wishIdFromUrl
+////        currentVC.navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
-
